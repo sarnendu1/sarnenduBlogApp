be05be14ffc2ac68b2c7af6af4381875d1759d18
Sarnendu Das <87897770+sarnendu1@users.noreply.github.com>
2022-01-31 13:00:14 +0530

    updated

---
+++ client/components/header/Header.jsx
@@ -0,0 +1,16 @@
+import Link from 'next/Link'
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="headerTitles">
+        <span className="headerTitleSm">My</span>
+        <span className="headerTitleLg">BLOG</span>
+      </div>
+      <img
+        className="headerImg"
+        src="https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        alt=""
+      />
+    </div>
+  );
+}
---
+++ client/components/sidebar/Sidebar.jsx
@@ -0,0 +1,63 @@
+import Link from 'next/Link'
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebarItem">
+        <span className="sidebarTitle">ABOUT ME</span>
+        <img
+          src="https://themegoods-cdn-pzbycso8wng.stackpathdns.com/grandblog/demo/wp-content/uploads/2015/11/aboutme.jpg"
+          alt=""
+        />
+        <p>
+          Laboris sunt aute cupidatat velit magna velit ullamco dolore mollit
+          amet ex esse.Sunt eu ut nostrud id quis proident.
+        </p>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">CATEGORIES</span>
+        <ul className="sidebarList">
+          <li className="sidebarListItem">
+            <Link className="link" href="/posts?cat=Life">
+              Life
+            </Link>
+          </li>
+          <li className="sidebarListItem">
+            <Link className="link" href="/posts?cat=Music">
+              Music
+            </Link>
+          </li>
+          <li className="sidebarListItem">
+            <Link className="link" href="/posts?cat=Sport">
+              Sport
+            </Link>
+          </li>
+          <li className="sidebarListItem">
+            <Link className="link" href="/posts?cat=Style">
+              Style
+            </Link>
+          </li>
+          <li className="sidebarListItem">
+            <Link className="link" href="/posts?cat=Tech">
+              Tech
+            </Link>
+          </li>
+          <li className="sidebarListItem">
+            <Link className="link" href="/posts?cat=Cinema">
+              Cinema
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">FOLLOW US</span>
+        <div className="sidebarSocial">
+          <i className="sidebarIcon fab fa-facebook-square"></i>
+          <i className="sidebarIcon fab fa-instagram-square"></i>
+          <i className="sidebarIcon fab fa-pinterest-square"></i>
+          <i className="sidebarIcon fab fa-twitter-square"></i>
+        </div>
+      </div>
+    </div>
+  );
+}
