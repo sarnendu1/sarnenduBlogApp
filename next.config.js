5cec0aeb683f543b509faa9e4be8a930821a8194
Sarnendu Das <87897770+sarnendu1@users.noreply.github.com>
2022-03-12 20:30:54 +0530

    updated

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  i18n: {
+    
+    locales: ['en-US', 'fr', 'nl-NL'],
+    
+    defaultLocale: 'en-US',
+  },
   reactStrictMode: true,
   images: {
     domains: ["images.pexels.com","i.pinimg.com","blogger.googleusercontent.com","drive.google.com","ibb.co"]
