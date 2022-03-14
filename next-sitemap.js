65c99a3cf664b01cadba50009621c05ce749e961
Sarnendu Das <87897770+sarnendu1@users.noreply.github.com>
2022-03-14 18:37:37 +0530

    updated

--- next-sitemap.js
+++ next-sitemap.js
@@ -2,6 +2,15 @@ const siteUrl = "https://www.purpleglib.in";
 
 module.exports = {
   siteUrl,
-  
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      { userAgent: "*", allow: "/" },
+    ],
+    additionalSitemaps: [
+      `${siteUrl}/static-sitemap.xml`,
+      `${siteUrl}/server-sitemap.xml`,
+    ],
+  },
  
 };
