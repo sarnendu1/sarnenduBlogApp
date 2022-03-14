7d1f7cc4ab5d596298b2667df26924647ff7ceab
Sarnendu Das <87897770+sarnendu1@users.noreply.github.com>
2022-03-14 18:13:23 +0530

    updated

--- next-sitemap.js
+++ next-sitemap.js
@@ -2,15 +2,6 @@ const siteUrl = "https://www.purpleglib.in";
 
 module.exports = {
   siteUrl,
-  generateRobotsTxt: true,
-  robotsTxtOptions: {
-    policies: [
-      { userAgent: "*", allow: "/" },
-    ],
-    additionalSitemaps: [
-      `${siteUrl}/sitemap.xml`,
-      `${siteUrl}/server-sitemap.xml`,
-    ],
-  },
+  
  
 };
