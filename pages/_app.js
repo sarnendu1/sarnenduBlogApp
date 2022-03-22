a6cac6c6a2e4bdc5ebbc0653f5f929c44c2efc5c
Sarnendu Das <87897770+sarnendu1@users.noreply.github.com>
2022-03-22 23:16:40 +0530

    updated

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,35 @@
 import '../styles/globals.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
+/* <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-KCR7Z3Z830"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'G-KCR7Z3Z830');
+</script>
+ */
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+  <Script strategy="lazyOnload" src="https://www.googletagmanager.com/gtag/js?id=G-KCR7Z3Z830" />
+  <Script strategy="lazyOnload">
+  {
+    `
+    window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-KCR7Z3Z830');
+  `
+  }
+  
+  </Script>  
+  <Component {...pageProps} />
+  </>
+  )
 }
 
 export default MyApp
