67c88df31d0bc73470775f64acf138ec193eeecb
Sarnendu Das <87897770+sarnendu1@users.noreply.github.com>
2022-03-22 19:52:46 +0530

    updated

--- pages/[post].js
+++ pages/[post].js
@@ -160,7 +160,7 @@ export default function SinglePost({data}) {
     <meta name="keywords" content={data.keywords} />
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet' />
     
-    <Script dangerouslySetInnerHTML={{ __html: `(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://iclickcdn.com/tag.min.js',4964243,document.body||document.documentElement)` }} />
+   
     <meta name="propeller" content="c90b9aaf2437c245c17d36d5b95a9b9d" />
     </Head>
     <Script async="async" data-cfasync="false" src="//pl17128038.effectivecpmcontent.com/a00d94f18094a0b051bc5d781da76fa3/invoke.js" />
@@ -275,6 +275,7 @@ export default function SinglePost({data}) {
   </div>
   </div>
     <Footer />
+    <Script dangerouslySetInnerHTML={{ __html: `(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://iclickcdn.com/tag.min.js',4964243,document.body||document.documentElement)` }} />
     <Script type='text/javascript' src='//pl17130463.effectivecpmcontent.com/1f/33/de/1f33de69dc7f152dc60061b2fedffc3a.js' />
     <Script src="https://kit.fontawesome.com/e48d166edc.js" crossorigin="anonymous" />
     </>
