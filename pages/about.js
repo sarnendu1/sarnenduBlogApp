4f7b67f98a4eea7de88ba56e7693fa3279a42e0f
Sarnendu Das <87897770+sarnendu1@users.noreply.github.com>
2022-03-25 16:49:54 +0530

    updated

--- pages/about.js
+++ pages/about.js
@@ -8,38 +8,12 @@ import {Navbar,Container,Nav,NavDropdown} from 'react-bootstrap';
 import Head from 'next/head';
 import { useEffect } from 'react';
 import { NextSeo } from 'next-seo';
+import { WhatsappIcon } from 'react-share';
+import {FaPhoneSquareAlt,FaEnvelopeSquare, FaWhatsappSquare,FaInstagram} from "react-icons/fa";
 
 const About = () => {
 
-  useEffect(() => {
-    let sidebar = document.querySelector(".sidebar");
-let closeBtn = document.querySelector("#btn");
-let searchBtn = document.querySelector(".bx-search");
-
-closeBtn.addEventListener("click", ()=>{
-sidebar.classList.toggle("open");
-menuBtnChange();//calling the function(optional)
-});
-
-searchBtn.addEventListener("click", ()=>{ // Sidebar open when you click on the search iocn
-sidebar.classList.toggle("open");
-menuBtnChange(); //calling the function(optional)
-});
-
-// following are the code to change sidebar button(optional)
-function menuBtnChange() {
-if(sidebar.classList.contains("open")){
- closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");//replacing the iocns class
-}else {
- closeBtn.classList.replace("bx-menu-alt-right","bx-menu");//replacing the iocns class
-}
-}
-
   
-    return () => {
-      
-    }
-  }, [])
   
   return (
   <>
@@ -88,110 +62,82 @@ if(sidebar.classList.contains("open")){
 </Navbar>
     <h1 className='about-heading'>About</h1>  
     <hr className='hr' />
-    <center>
+   
+   <div className='flex'>
+      <div>
+    
     <Container className='hero'>
-    <Image
+   
+
+    <div className='about-desc-box'>
+    <p className='about-desc'>Hi Friends,Welcome to Purple Glib, It's a blog and codes website, Here you will find tech blogs and free codes of designs and projects which all written and coded by Mr.Sarnendu Das(Founder). He is a Professional Web Developer with a passion for Web Designing And Developing, and he also Makes Videos On Youtube.
+
+He started Purple Glib with the idea of helping Students like yourself, by giving some Tech and Educational blogs, Free codes and Youtube Videos.
+
+He knows many programming languages like `<i> Html,Css,Javascript,Next Js,React Js,Node Js,Express Js,MongoDB,Php And Mysql</i>``.
+
+His Youtube Channels Names Are Sarnendu Official And Career Champions, You Can Visit his Channel And Check his Videos And If You Like his Videos Then You Can Subscribe.</p>
+</div>
+
+</Container>
+
+  </div>
+
+  <div className='author-card'>
+    <div className='author'>
+      <center>
+  <Image
     src="/hero.png"
     width={280}
     height={250}
     alt="Sarnendu Das"
+    className='hero-img'
     />
+    <h3 className='mt-2'>Sarnendu Das</h3>
+    <p>Founder</p>
+    <h5>Follow</h5>
     <div>
     <ul className='social'>
         
         <li>
-            <Link href="https://www.facebook.com/sarnendu28"><a className="navlinkfacebook" ><FaFacebookSquare size={50}/></a></Link>
+            <Link href="https://www.facebook.com/sarnendu28"><a className="navlinkfacebook" ><FaFacebookSquare size={40}/></a></Link>
         </li>
         <li>
-            <Link href="/about"><a className="navlinkyoutube"><FaYoutubeSquare size={50}/></a></Link>
+            <Link href="/about"><a className="navlinkyoutube"><FaYoutubeSquare size={40}/></a></Link>
+        </li>
+
+        <li>
+            <Link href="/about"><a className="navlinkyoutube"><FaInstagram size={40}/></a></Link>
         </li>
         
         <li>
-            <Link href="/about"><a className="navlinklinkedin" ><FaLinkedin size={50}/></a></Link>
+            <Link href="/about"><a className="navlinklinkedin" ><FaLinkedin size={40}/></a></Link>
+        </li>
+
+      </ul>
+
+      <h5>Contact</h5>
+      <ul className='social'>
+        
+        <li>
+            <Link href="tel:+918013024559"><a className="navlinkyoutube"><FaPhoneSquareAlt size={40}/></a></Link>
+        </li>
+        <li>
+            <Link href="mailto:sarnendu1@gmail.com"><a className="navlinkyoutube"><FaEnvelopeSquare size={40}/></a></Link>
+        </li>
+        
+
+        <li>
+            <Link href="https://wa.me/+918013024559"><a className="navlinkyoutube"><FaWhatsappSquare size={40}/></a></Link>
         </li>
       </ul>
+
       </div>
+    </center>
 
-    <div className='about-desc-box'>
-    <p className='about-desc'>Hi Friends,My name is Sarnendu, I am a Web Developer with a passion for Web Designing And Developing, Making Videos On Youtube.
-
-I started Sarnendu with the idea of helping Students like yourself, by giving some Tech and Educational blogs and Youtube Videos.
-
-I know many programming languages like `<i> Html,Css,Javascript,Next Js,React Js,Node Js,Express Js,MongoDB,Php And Mysql</i>``.
-
-Currently I am Working As A Web Developer
-
-My Youtube Channels Names Are Sarnendu Official And Career Champions, You Can Visit My Channel And Check My Videos And If You Like My Videos Then You Can Subscribe.</p>
-</div>
-
-</Container>
-</center>
-<div className="sidebar-display">
-<div className="sidebar">
-    <div className="logo-details">
-      {/* <i class='bx bxl-c-plus-plus icon'></i> */}
-        {/* <div class="logo_name">CodingLab</div> */}
-        <i className='bx bx-menu' id="btn" ></i>
-    </div>
-    <ul className="nav-list">
-      <li>
-          <i className='bx bx-search' ></i>
-         <input type="text" placeholder="Search..." onChange={(e)=>search(e.target.value)}/>
-         <span className="tooltip">Search</span>
-      </li>
-      <li>
-        <Link href="/about">
-        <a>
-        <i className='bx bx-user-pin' ></i>
-          <span className="links_name">About</span>
-        
-         <span className="tooltip">About</span>
-         </a>
-         </Link>
-      </li>
-      
-     <li>
-     <Link href="/contact">
-       <a>
-       <i className='bx bxs-contact' ></i>
-         <span className="links_name">Contact</span>
-       
-       <span className="tooltip">Contact</span>
-       </a>
-       </Link>
-     </li>
-     <li>
-       <Link href="/categories/blog">
-       <a>
-       <i className='bx bx-pen'></i>
-         <span className="links_name">Blog</span>
-       
-       <span className="tooltip">Blog</span>
-       </a>
-       </Link>
-     </li>
-     <li>
-       <Link href="/categories/codes">
-       <a>
-       <i className='bx bx-code-alt'></i>
-         <span className="links_name">Codes</span>
-       
-       <span className="tooltip">Codes</span>
-       </a>
-       </Link>
-     </li>
-     <li>
-       <Link href="/categories/youtube" >
-       <a>
-       <i className='bx bxl-youtube' ></i>
-         <span className="links_name">Youtube</span>
-     
-       <span className="tooltip">Youtube</span>
-       </a>
-       </Link>
-     </li>
-    </ul>
   </div>
+  </div>
+
   </div>
     <Footer />
   </>
